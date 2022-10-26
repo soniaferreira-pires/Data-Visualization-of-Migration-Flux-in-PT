a1de67d8b8d9320abbeaf6d5a0c35aab9e81557b
Cátia Teixeira <catia.rds.teixeira@gmail.com>
2022-10-26 10:11:17 +0100

    Started script
    Start of project script: upload libraries, read csv, first graph.
    
    Signed-off-by: Cátia Teixeira <catia.rds.teixeira@gmail.com>

--- VPDA_project_CT_VB_SF.R
+++ VPDA_project_CT_VB_SF.R
@@ -1,1 +1,16 @@
-# trabalhinho
+setwd('C:/Users/Asus/Documents/FEUP/MDSE/VPD/VPDA project')
+
+library(tidyverse)
+
+emigration <- read_csv2("emigration.csv") 
+imigration <- read_csv2("imigration.csv") 
+
+
+e <- ggplot(data = emigration) + geom_point(mapping = aes(x = Anos, y = Total))
+
+i <- ggplot(data = imigration) + geom_point(mapping = aes(x = Anos, y = Total))
+
+ggplot(mapping = aes(x = Anos, y = Total)) + 
+  geom_point(data = emigration, color='darkgreen') +
+  geom_point(data = imigration, color = 'orange') +
+  theme_minimal()
